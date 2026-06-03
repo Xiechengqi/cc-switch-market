b05b78ad3c20c2989370c5ec76ab23034da53bd4
root <root@git.com>
2026-06-03 13:41:01 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+rm -f -v cc-switch-market
+
 (cd web && pnpm install --frozen-lockfile && pnpm build)
 cargo build --release
+
 cp -f -v target/release/cc-switch-market .
